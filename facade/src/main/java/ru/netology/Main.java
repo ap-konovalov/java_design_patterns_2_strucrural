7fc3f2e7a0794b7cae380ddd99e3a49f07a2a751
APKonovalov <APKonovalov@alfabank.ru>
2023-07-09 12:19:03 +0300

    initial commit

---
+++ facade/src/main/java/ru/netology/Main.java
@@ -0,0 +1,10 @@
+package ru.netology;
+
+// https://github.com/netology-code/jd-homeworks/blob/master/structural/task2/README.md
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.sum("10","11"));
+        System.out.println(binOps.mult("10","11"));
+    }
+}
